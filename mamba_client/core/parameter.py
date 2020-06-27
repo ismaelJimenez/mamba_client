2be996bf7c69d9a3f806b59168cb825b620ff9d0
Ismael <barracuda.framework@gmail.com>
2020-06-27 20:15:14 +0200

    added parameter

---
+++ mamba_client/core/parameter.py
@@ -0,0 +1,69 @@
+from typing import Optional, Any
+
+from mamba_client.core.network_controller import NetworkController
+from mamba_client.core.parameter_set import ParameterSet
+from mamba_client.core.parameter_get import ParameterGet
+
+
+class Parameter:
+    def __init__(self,
+                 parameter_id: str,
+                 network_controller: NetworkController,
+                 get_alias: Optional[str] = None,
+                 set_alias: Optional[str] = None,
+                 set_arg_1: Optional[Any] = None,
+                 set_arg_2: Optional[Any] = None,
+                 set_arg_3: Optional[Any] = None,
+                 set_arg_4: Optional[Any] = None,
+                 set_arg_5: Optional[Any] = None,
+                 set_arg_6: Optional[Any] = None,
+                 set_arg_7: Optional[Any] = None,
+                 set_arg_8: Optional[Any] = None,
+                 set_arg_9: Optional[Any] = None,
+                 set_arg_10: Optional[Any] = None) -> None:
+        self._parameter_id = parameter_id
+        self._get = ParameterGet(network_controller,
+                                 get_alias) if get_alias is not None else None
+        self._set = ParameterSet(
+            network_controller,
+            set_alias,
+            arg_1=set_arg_1,
+            arg_2=set_arg_2,
+            arg_3=set_arg_3,
+            arg_4=set_arg_4,
+            arg_5=set_arg_5,
+            arg_6=set_arg_6,
+            arg_7=set_arg_7,
+            arg_8=set_arg_8,
+            arg_9=set_arg_9,
+            arg_10=set_arg_10) if set_alias is not None else None
+
+    @property
+    def id(self) -> str:
+        """ ID of the parameter """
+        return self._parameter_id
+
+    def get(self):
+        if self._get is not None:
+            return self._get()
+
+        raise RuntimeError(
+            f'Parameter {self._parameter_id} GET command is not defined.')
+
+    def set(self,
+            arg_1: Optional[Any] = None,
+            arg_2: Optional[Any] = None,
+            arg_3: Optional[Any] = None,
+            arg_4: Optional[Any] = None,
+            arg_5: Optional[Any] = None,
+            arg_6: Optional[Any] = None,
+            arg_7: Optional[Any] = None,
+            arg_8: Optional[Any] = None,
+            arg_9: Optional[Any] = None,
+            arg_10: Optional[Any] = None):
+        if self._set is not None:
+            return self._set(arg_1, arg_2, arg_3, arg_4, arg_5, arg_6, arg_7,
+                             arg_8, arg_9, arg_10)
+
+        raise RuntimeError(
+            f'Parameter {self._parameter_id} SET command is not defined.')
---
+++ tests/core/test_parameter.py
@@ -0,0 +1,116 @@
+import pytest
+
+from mamba_client.core.network_controller import NetworkController
+from mamba_client.core.parameter import Parameter
+from mamba_client.mock.mamba_server_mock import MambaServerMock
+from mamba_client.core.exceptions import ParameterSetException
+
+
+class TestClass:
+    def test_parameter_init(self):
+        MambaServerMock(port=34572)
+        network_controller = NetworkController(port=34572)
+
+        param = Parameter(parameter_id='test_1',
+                          network_controller=network_controller,
+                          get_alias='test_1_get',
+                          set_alias='test_1_set')
+
+        assert param._parameter_id == 'test_1'
+        assert param._get is not None
+        assert param._set is not None
+
+        param = Parameter(parameter_id='test_1',
+                          network_controller=network_controller,
+                          set_alias='test_1_set')
+
+        assert param._parameter_id == 'test_1'
+        assert param._get is None
+        assert param._set is not None
+
+        param = Parameter(parameter_id='test_1',
+                          network_controller=network_controller,
+                          get_alias='test_1_get')
+
+        assert param._parameter_id == 'test_1'
+        assert param._get is not None
+        assert param._set is None
+
+        network_controller.close()
+
+    def test_parameter_call(self):
+        mock = MambaServerMock(port=34573)
+        network_controller = NetworkController(port=34573)
+
+        param = Parameter(parameter_id='test_1',
+                          network_controller=network_controller,
+                          get_alias='test_1_get',
+                          set_alias='test_1_set')
+
+        assert mock._shared_memory['last_tc'] == 'tc_meta test_1_set'
+        assert mock._shared_memory[
+            'last_tm'] == '> OK test_1_set;1;test description'
+
+        reply = param.get()
+
+        assert mock._shared_memory['last_tc'] == 'tm test_1_get'
+        assert '> OK test_1_get;' in mock._shared_memory['last_tm']
+        assert ';1;1;0;1' in mock._shared_memory['last_tm']
+        assert reply == '1'
+
+        param.set(1)
+
+        assert mock._shared_memory['last_tc'] == 'tc test_1_set "1"'
+        assert mock._shared_memory['last_tm'] == '> OK test_1_set'
+
+        with pytest.raises(ParameterSetException) as excinfo:
+            param.set(1, 2)
+
+        assert str(
+            excinfo.value
+        ) == 'test_1_set set - Wrong number of arguments. Expected: 1, Received: 2.'
+
+        param = Parameter(parameter_id='test_2',
+                          network_controller=network_controller,
+                          get_alias='test_2_get',
+                          set_alias='test_2_set',
+                          set_arg_2='22')
+
+        reply = param.get()
+
+        assert mock._shared_memory['last_tc'] == 'tm test_2_get'
+        assert '> OK test_2_get;' in mock._shared_memory['last_tm']
+        assert ';1;1;0;1' in mock._shared_memory['last_tm']
+        assert reply == '1'
+
+        param.set(1)
+
+        assert mock._shared_memory['last_tc'] == 'tc test_2_set "1" "22"'
+        assert mock._shared_memory['last_tm'] == '> OK test_2_set'
+
+        with pytest.raises(ParameterSetException) as excinfo:
+            param.set()
+
+        assert str(
+            excinfo.value
+        ) == 'test_2_set set - Wrong number of arguments. Expected: 2, Received: 0.'
+
+        param = Parameter(parameter_id='test_1',
+                          network_controller=network_controller,
+                          get_alias='test_1_get',
+                          set_alias='test_1_set',
+                          set_arg_1=1)
+
+        reply = param.get()
+
+        assert mock._shared_memory['last_tc'] == 'tm test_1_get'
+        assert '> OK test_1_get;' in mock._shared_memory['last_tm']
+        assert ';1;1;0;1' in mock._shared_memory['last_tm']
+        assert reply == '1'
+
+        param.set()
+
+        assert mock._shared_memory['last_tc'] == 'tc test_1_set "1"'
+        assert mock._shared_memory['last_tm'] == '> OK test_1_set'
+
+        network_controller.close()
